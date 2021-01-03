cbe55388358a619c5c25dc40a291618b13698f80
BAKAOLC <2565128043@qq.com>
2021-01-04 06:30:48 +0800

    我为什么要加这个

--- Ritsukage-Core/Commands/Time.cs
+++ Ritsukage-Core/Commands/Time.cs
@@ -25,11 +25,25 @@ namespace Ritsukage.Commands
                 await pm.Reply(msg);
         }
 
+        static long GetEorzeaHour(long unix) => unix / 175 % 24;
+        static long GetEorzeaMinute(long unix) => Convert.ToInt64(60 * ((double)unix / 175 % 1));
+
+        [Command("艾欧泽亚时间", "et")]
+        public static async void ET(BaseSoraEventArgs e)
+        {
+            long unix = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+            string msg = $"当前为艾欧泽亚时间：ET {GetEorzeaHour(unix),2:D2}:{GetEorzeaMinute(unix),2:D2}";
+            if (e is GroupMessageEventArgs gm)
+                await gm.Reply(msg);
+            else if (e is PrivateMessageEventArgs pm)
+                await pm.Reply(msg);
+        }
+
         [Command("北欧历")]
         public static async void BOL(BaseSoraEventArgs e)
         {
             var day = Math.Floor((DateTime.Now - new DateTime(2019, 8, 1, 0, 0, 0)).TotalDays) + 1;
-            string msg = $"当前为北欧历时间：\n2019年08月{day,2}日 " + DateTime.Now.ToString("HH时mm分ss秒");
+            string msg = $"当前为北欧历时间：\n2019年08月{day,2:D2}日 " + DateTime.Now.ToString("HH时mm分ss秒");
             if (e is GroupMessageEventArgs gm)
                 await gm.Reply(msg);
             else if (e is PrivateMessageEventArgs pm)
@@ -40,7 +54,7 @@ namespace Ritsukage.Commands
         public static async void NewBOL(BaseSoraEventArgs e)
         {
             var day = Math.Floor((DateTime.Now - new DateTime(2020, 6, 1, 0, 0, 0)).TotalDays) + 1;
-            string msg = $"当前为新北欧历时间：\n2020年06月{day,2}日 " + DateTime.Now.ToString("HH时mm分ss秒");
+            string msg = $"当前为新北欧历时间：\n2020年06月{day,2:D2}日 " + DateTime.Now.ToString("HH时mm分ss秒");
             if (e is GroupMessageEventArgs gm)
                 await gm.Reply(msg);
             else if (e is PrivateMessageEventArgs pm)
