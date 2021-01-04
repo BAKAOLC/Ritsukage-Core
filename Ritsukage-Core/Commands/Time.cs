e3b996c02056d6354e4b561b70dffcc0b0a40994
BAKAOLC <2565128043@qq.com>
2021-01-04 10:24:53 +0800

    我晕了

--- Ritsukage-Core/Commands/Time.cs
+++ Ritsukage-Core/Commands/Time.cs
@@ -43,7 +43,7 @@ namespace Ritsukage.Commands
         public static async void BOL(BaseSoraEventArgs e)
         {
             var day = Math.Floor((DateTime.Now - new DateTime(2019, 8, 1, 0, 0, 0)).TotalDays) + 1;
-            string msg = $"当前为北欧历时间：\n2019年08月{day,2:D2}日 " + DateTime.Now.ToString("HH时mm分ss秒");
+            string msg = $"当前为北欧历时间：\n2019年08月{day,2}日 " + DateTime.Now.ToString("HH时mm分ss秒");
             if (e is GroupMessageEventArgs gm)
                 await gm.Reply(msg);
             else if (e is PrivateMessageEventArgs pm)
@@ -54,7 +54,27 @@ namespace Ritsukage.Commands
         public static async void NewBOL(BaseSoraEventArgs e)
         {
             var day = Math.Floor((DateTime.Now - new DateTime(2020, 6, 1, 0, 0, 0)).TotalDays) + 1;
-            string msg = $"当前为新北欧历时间：\n2020年06月{day,2:D2}日 " + DateTime.Now.ToString("HH时mm分ss秒");
+            string msg = $"当前为新北欧历时间：\n2020年06月{day,2}日 " + DateTime.Now.ToString("HH时mm分ss秒");
+            if (e is GroupMessageEventArgs gm)
+                await gm.Reply(msg);
+            else if (e is PrivateMessageEventArgs pm)
+                await pm.Reply(msg);
+        }
+
+        [Command("日期测试")]
+        public static async void DateTimeTest(BaseSoraEventArgs e, DateTime dt)
+        {
+            string msg = dt.ToString("yyyy年MM月dd日 HH时mm分ss秒");
+            if (e is GroupMessageEventArgs gm)
+                await gm.Reply(msg);
+            else if (e is PrivateMessageEventArgs pm)
+                await pm.Reply(msg);
+        }
+
+        [Command("时间测试")]
+        public static async void TimeSpanTest(BaseSoraEventArgs e, TimeSpan ts)
+        {
+            string msg = ts.ToString();
             if (e is GroupMessageEventArgs gm)
                 await gm.Reply(msg);
             else if (e is PrivateMessageEventArgs pm)
--- Ritsukage-Core/Tools/DateTimeReader.cs
+++ Ritsukage-Core/Tools/DateTimeReader.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Globalization;
 
 namespace Ritsukage.Tools
@@ -6,25 +7,25 @@ namespace Ritsukage.Tools
     public static class DateTimeReader
     {
         static readonly string[] DateFormats = {
-            "yyyy'年'MM'月'dd'日'",     //2021年01月04日
-            "yyyy'年'MM'月'",          //2021年01月
-            "yyyy'年'",                //2021年
-            "MM'月'dd'日'",            //01月04日
-            "MM'月'",                  //01月
-            "dd'日'",                  //04日
-            "yyyy'-'MM'-'dd",          //2021-01-04
-            "yyyy'-'MM'",              //2021-01
-            "MM'-'dd",                 //01-04
-            "yyyy'/'MM'/'dd",          //2021/01/04
-            "yyyy'/'MM",               //2021/01
-            "MM'/'dd",                 //01/04
+            "%y'年'%M'月'%d'日'",     //2021年01月04日
+            "%y'年'%M'月'",          //2021年01月
+            "%y'年'",                //2021年
+            "%M'月'%d'日'",            //01月04日
+            "%M'月'",                  //01月
+            "%d'日'",                  //04日
+            "%y'-'%M'-'%d",          //2021-01-04
+            "%y'-'%M'",              //2021-01
+            "%M'-'%d",                 //01-04
+            "%y'/'%M'/'%d",          //2021/01/04
+            "%y'/'%M",               //2021/01
+            "%M'/'%d",                 //01/04
         };
         static readonly string[] TimeFormats = {
-            "HH'时'mm'分'ss'秒'", //08时41分20秒
-            "HH'时'mm'分'", //08时41分
-            "mm'分'ss'秒'", //41分20秒
-            "HH':'mm':'ss", //08:41:20
-            "HH':'mm", //08:41
+            "%H'时'%m'分'%s'秒'", //08时41分20秒
+            "%H'时'%m'分'", //08时41分
+            "%m'分'%s'秒'", //41分20秒
+            "%H':'%m':'%s", //08:41:20
+            "%H':'%m", //08:41
         };
 
         public static DateTime Parse(string original)
@@ -36,6 +37,7 @@ namespace Ritsukage.Tools
                 Date = gotDate.Date;
             if (DateTime.TryParseExact(s, TimeFormats, CultureInfo.InvariantCulture, DateTimeStyles.AllowWhiteSpaces, out var gotTime))
                 Time = gotTime.TimeOfDay;
+
             if (Date.HasValue && Time.HasValue)
                 return Date.Value + Time.Value;
             else if (Date.HasValue)
