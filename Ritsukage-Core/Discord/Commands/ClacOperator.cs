ebaab823f688c07eb52809132a8a216c59404dd2
BAKAOLC <Ritsukage@qq.com>
2021-02-14 15:45:35 +0800

    整理CalcOperator代码，并增加Discord方适配

---
+++ Ritsukage-Core/Discord/Commands/ClacOperator.cs
@@ -0,0 +1,22 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace Ritsukage.Discord.Commands
+{
+    public class ClacOperator : ModuleBase<SocketCommandContext>
+    {
+        [Command("calc")]
+        public async Task CalcMath(string expr)
+        {
+            try
+            {
+                double result = Tools.CalcTool.GetExprValue(expr.Replace(" ", ""));
+                await ReplyAsync($"{expr} = {result}");
+            }
+            catch
+            {
+                await ReplyAsync("操作失败");
+            }
+        }
+    }
+}
---
+++ Ritsukage-Core/Tools/CalcTool.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ritsukage.Tools
+{
+    public static class CalcTool
+    {
+        enum Operator
+        {
+            ADD, SUB, MUL, DIV,
+            LB, RB, MOD
+        }
+
+        static int GetOperatorPriority(Operator op)
+            => op switch
+            {
+                Operator.ADD or Operator.SUB => 0,
+                Operator.MUL or Operator.DIV => 1,
+                Operator.LB or Operator.RB => 9961,
+                Operator.MOD => 1,
+                _ => 0,
+            };
+
+        static double OpNum(Operator op, double a, double b)
+            => op switch
+            {
+                Operator.ADD => a + b,
+                Operator.SUB => a - b,
+                Operator.MUL => a * b,
+                Operator.DIV => a / b,
+                Operator.MOD => a % b,
+                _ => throw new ArgumentOutOfRangeException($"operator {op} is not suppported calc"),
+            };
+
+        static Operator? GetOperator(char c)
+            => c switch
+            {
+                '+' => Operator.ADD,
+                '-' => Operator.SUB,
+                'x' or 'X' or '*' => Operator.MUL,
+                '/' => Operator.DIV,
+                '(' => Operator.LB,
+                ')' => Operator.RB,
+                '%' => Operator.MOD,
+                _ => null,
+            };
+
+        static double ParseNum(string n)
+            => n.ToLower() switch
+            {
+                "pi" or "Π" => Math.PI,
+                "e" => Math.E,
+                _ => double.Parse(n),
+            };
+
+        /// <summary>
+        /// 从开始地点解析到第一个()结束的值
+        /// </summary>
+        /// <param name="expr">字符串表达式</param>
+        /// <param name="idx">字符串表达式解析开始点</param>
+        /// <returns>(表达式的值, 结束时的索引位置(包含))</returns>
+        static (double, int) ParseUnitValue(string expr, int start)
+        {
+            int idx = start;
+            int brackets = 0;
+            bool internalParse = true;
+            while (idx < expr.Length && internalParse)
+            {
+                var possibleOp = GetOperator(expr[idx]);
+                if (possibleOp != null && possibleOp.HasValue)
+                {
+                    switch (possibleOp.Value)
+                    {
+                        case Operator.LB:
+                            brackets += 1;
+                            break;
+                        case Operator.RB:
+                            brackets -= 1;
+                            if (brackets == 0)
+                            {
+                                internalParse = false;
+                            }
+                            break;
+                    }
+                }
+                idx += 1;
+            }
+            double value = GetExprValue(expr[start..idx]);
+            return (value, idx - 1);
+        }
+
+        public static double GetExprValue(string expr)
+        {
+            var nums = new Stack<double>();
+            var ops = new Stack<Operator>();
+            int idx = 0;
+            int start = 0;
+            while (idx < expr.Length)
+            {
+                char c = expr[idx];
+                var op = GetOperator(c);
+                //what is c# nullable mean??????
+                //op != null mean the nullable is not null or has value?
+                if (op != null && op.HasValue)
+                {
+                    //maybe sqrt(.... or sth else
+                    if (op.Value == Operator.LB)
+                    {
+                        if (start != idx)
+                        {
+                            var v = expr[start..idx];
+                            switch (v)
+                            {
+                                case "sqrt":
+                                    {
+                                        //point '(' at begin
+                                        var (numV, endIdx) = ParseUnitValue(expr, idx);
+                                        nums.Push(Math.Sqrt(numV));
+                                        idx = endIdx;
+                                        start = idx + 1;
+                                        break;
+                                    }
+                                case "sin":
+                                    {
+                                        var (numV, endIdx) = ParseUnitValue(expr, idx);
+                                        nums.Push(Math.Sin(numV));
+                                        idx = endIdx;
+                                        start = idx + 1;
+                                        break;
+                                    }
+                                case "cos":
+                                    {
+                                        var (numV, endIdx) = ParseUnitValue(expr, idx);
+                                        nums.Push(Math.Cos(numV));
+                                        idx = endIdx;
+                                        start = idx + 1;
+                                        break;
+                                    }
+                                case "tan":
+                                    {
+                                        var (numV, endIdx) = ParseUnitValue(expr, idx);
+                                        nums.Push(Math.Tan(numV));
+                                        idx = endIdx;
+                                        start = idx + 1;
+                                        break;
+                                    }
+                                case "abs":
+                                    {
+                                        var (numV, endIdx) = ParseUnitValue(expr, idx);
+                                        nums.Push(Math.Abs(numV));
+                                        idx = endIdx;
+                                        start = idx + 1;
+                                        break;
+                                    }
+                                default:
+                                    {
+                                        nums.Push(ParseNum(expr[start..idx]));
+                                        ops.Push(op.Value);
+                                        start = idx += 1;
+                                        break;
+                                    }
+                            }
+                        }
+                        else
+                        {
+                            start = idx + 1;
+                            ops.Push(op.Value);
+                        }
+                    }
+                    else
+                    {
+                        if (start != idx)
+                        {
+                            var v = expr[start..idx];
+                            nums.Push(ParseNum(v));
+                        }
+                        if (op.Value == Operator.RB)
+                        {
+                            var lastOp = ops.Pop();
+                            while (lastOp != Operator.LB)
+                            {
+                                double b = nums.Pop();
+                                double a = nums.Pop();
+                                nums.Push(OpNum(lastOp, a, b));
+                                lastOp = ops.Pop();
+                            }
+                            start = idx + 1;
+                        }
+                        else
+                        {
+                            if (!(op == Operator.SUB && start == idx))
+                            {
+                                if (ops.Count > 0)
+                                {
+                                    var lastOp = ops.Peek();
+                                    while (lastOp != Operator.LB && GetOperatorPriority(op.Value) < GetOperatorPriority(lastOp))
+                                    {
+                                        double b = nums.Pop();
+                                        double a = nums.Pop();
+                                        nums.Push(OpNum(lastOp, a, b));
+                                        ops.Pop();
+                                        if (ops.Count == 0)
+                                        {
+                                            break;
+                                        }
+                                        lastOp = ops.Peek();
+                                    }
+                                }
+                                ops.Push(op.Value);
+                                start = idx + 1;
+                            }
+                        }
+                    }
+                }
+                idx += 1;
+            }
+            if (idx != start)
+                nums.Push(ParseNum(expr[start..]));
+            while (ops.TryPop(out var op))
+            {
+                double b = nums.Pop();
+                double a = nums.Pop();
+                nums.Push(OpNum(op, a, b));
+            }
+            if (ops.Count == 1 && nums.Count == 1 && ops.Pop() == Operator.SUB)
+                nums.Push(-nums.Pop());
+            return nums.Pop();
+        }
+    }
+}
