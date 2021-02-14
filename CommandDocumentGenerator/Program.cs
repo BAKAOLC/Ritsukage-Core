1dc0e9b2c6268a74e24de2f999b78bc025401538
BAKAOLC <Ritsukage@qq.com>
2021-02-15 00:51:47 +0800

    初始化doc生成器工程，补充忽略文件列表

---
+++ CommandDocumentGenerator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+
+namespace CommandDocumentGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type[] types = Assembly.GetAssembly(typeof(Ritsukage.QQ.Commands.Command)).GetExportedTypes();
+        }
+    }
+}
