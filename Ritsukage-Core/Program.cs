2fe8bbad092f2ff100a3b5a47002fd3a500ef611
BAKAOLC <ritsukage@qq.com>
2022-10-07 03:35:25 +0800

    SimpleWatchDog

--- Ritsukage-Core/Program.cs
+++ Ritsukage-Core/Program.cs
@@ -1,7 +1,6 @@
 ﻿using Meowtrix.PixivApi;
 using Meowtrix.PixivApi.Json;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using Ritsukage.Discord;
 using Ritsukage.Library.Data;
 using Ritsukage.Library.Subscribe;
@@ -19,163 +18,157 @@ namespace Ritsukage
 {
     class Program
     {
-        public static QQService QQServer { get; private set; }
-        public static DiscordAPP DiscordServer { get; private set; }
-
-        public static PixivApiClient PixivApi { get; private set; }
-        public static string PixivApiToken => PixivApi == null ? string.Empty : PixivApiAuthResponse.AccessToken;
-        static DateTimeOffset PixivApiAuthTime { get; set; }
-        static AuthResponse PixivApiAuthResponse { get; set; }
-        static DateTimeOffset PixivApiAuthExpiresIn { get; set; }
-
-        static void UpdatePixivApiToken(DateTimeOffset authTime, AuthResponse authResponse)
+        static class PixivAccountClient
         {
-            PixivApiAuthTime = authTime;
-            PixivApiAuthResponse = authResponse;
-            PixivApiAuthExpiresIn = authTime.AddSeconds(authResponse.ExpiresIn);
-            SavePixivApiToken();
-        }
+            public static PixivApiClient PixivApi { get; private set; }
+            public static string PixivApiToken => PixivApi == null ? string.Empty : PixivApiAuthResponse.AccessToken;
+            static DateTimeOffset PixivApiAuthTime { get; set; }
+            static AuthResponse PixivApiAuthResponse { get; set; }
+            static DateTimeOffset PixivApiAuthExpiresIn { get; set; }
 
-        static void SavePixivApiToken()
-        {
-            if (PixivApiAuthResponse != null)
-                File.WriteAllText("pixiv_refresh_token", PixivApiAuthResponse.RefreshToken);
-        }
-
-        static async Task<bool> LoginWithLastAuthToken(PixivApiClient pixiv_api)
-        {
-            try
+            static void UpdatePixivApiToken(DateTimeOffset authTime, AuthResponse authResponse)
             {
-                if (File.Exists("pixiv_refresh_token"))
+                PixivApiAuthTime = authTime;
+                PixivApiAuthResponse = authResponse;
+                PixivApiAuthExpiresIn = authTime.AddSeconds(authResponse.ExpiresIn);
+                SavePixivApiToken();
+            }
+
+            static void SavePixivApiToken()
+            {
+                if (PixivApiAuthResponse != null)
+                    File.WriteAllText("pixiv_refresh_token", PixivApiAuthResponse.RefreshToken);
+            }
+
+            static async Task<bool> LoginWithLastAuthToken(PixivApiClient pixiv_api)
+            {
+                try
                 {
-                    var token = File.ReadAllText("pixiv_refresh_token");
-                    (var authTime, var authResponse) = await pixiv_api.AuthAsync(token);
-                    UpdatePixivApiToken(authTime, authResponse);
-                    return true;
+                    if (File.Exists("pixiv_refresh_token"))
+                    {
+                        var token = File.ReadAllText("pixiv_refresh_token");
+                        (var authTime, var authResponse) = await pixiv_api.AuthAsync(token);
+                        UpdatePixivApiToken(authTime, authResponse);
+                        return true;
+                    }
                 }
-            }
-            catch (Exception ex)
-            {
-                ConsoleLog.Error("Pixiv", ex.GetFormatString());
-            }
-            return false;
-        }
-
-        static async void PixivApiLogin()
-        {
-            var pixiv_api = new PixivApiClient();
-            if (await LoginWithLastAuthToken(pixiv_api))
-            {
-                KeepPixivApiAuth();
-                PixivApi = pixiv_api;
-                ConsoleLog.Info("Main", "Pixiv Api 已初始化");
-                return;
-            }
-            else
-            {
-                (string verify, string url) = pixiv_api.BeginAuth();
-                File.WriteAllText("PixivLoginUrl.txt", "请在浏览器中打开并登录Pixiv，然后在F12的Network页面中获取其中 pixiv://....?code=xxx 的xxx部分粘贴在程序中" + Environment.NewLine + url);
-                Process.Start("notepad.exe", "PixivLoginUrl.txt");
-                await Task.Factory.StartNew(async () =>
+                catch (Exception ex)
                 {
-                    string key = Console.ReadLine();
-                    try
+                    ConsoleLog.Error("Pixiv", ex.GetFormatString());
+                }
+                return false;
+            }
+
+            public static async void PixivApiLogin()
+            {
+                var pixiv_api = new PixivApiClient();
+                if (await LoginWithLastAuthToken(pixiv_api))
+                {
+                    KeepPixivApiAuth();
+                    PixivApi = pixiv_api;
+                    ConsoleLog.Info("Main", "Pixiv Api 已初始化");
+                    return;
+                }
+                else
+                {
+                    (string verify, string url) = pixiv_api.BeginAuth();
+                    File.WriteAllText("PixivLoginUrl.txt", "请在浏览器中打开并登录Pixiv，然后在F12的Network页面中获取其中 pixiv://....?code=xxx 的xxx部分粘贴在程序中" + Environment.NewLine + url);
+                    Process.Start("notepad.exe", "PixivLoginUrl.txt");
+                    await Task.Factory.StartNew(async () =>
                     {
-                        File.Delete("PixivLoginUrl.txt");
-                    }
-                    catch
-                    {
-                    }
-                    if (string.IsNullOrEmpty(key))
-                    {
-                        ConsoleLog.Error("Pixiv", "Pixiv Api 已禁用，将在下一次启动时重新登录");
-                    }
-                    else
-                    {
+                        string key = Console.ReadLine();
                         try
                         {
-                            (var authTime, var authResponse) = await pixiv_api.CompleteAuthAsync(key, verify);
-                            UpdatePixivApiToken(authTime, authResponse);
-                            KeepPixivApiAuth();
-                            PixivApi = pixiv_api;
-                            ConsoleLog.Info("Main", "Pixiv Api 已初始化");
+                            File.Delete("PixivLoginUrl.txt");
                         }
-                        catch (Exception ex)
+                        catch
                         {
-                            ConsoleLog.Error("Pixiv", ex.GetFormatString());
+                        }
+                        if (string.IsNullOrEmpty(key))
+                        {
                             ConsoleLog.Error("Pixiv", "Pixiv Api 已禁用，将在下一次启动时重新登录");
+                        }
+                        else
+                        {
+                            try
+                            {
+                                (var authTime, var authResponse) = await pixiv_api.CompleteAuthAsync(key, verify);
+                                UpdatePixivApiToken(authTime, authResponse);
+                                KeepPixivApiAuth();
+                                PixivApi = pixiv_api;
+                                ConsoleLog.Info("Main", "Pixiv Api 已初始化");
+                            }
+                            catch (Exception ex)
+                            {
+                                ConsoleLog.Error("Pixiv", ex.GetFormatString());
+                                ConsoleLog.Error("Pixiv", "Pixiv Api 已禁用，将在下一次启动时重新登录");
+                            }
+                        }
+                    });
+                }
+            }
+
+            static void KeepPixivApiAuth()
+            {
+                Task.Factory.StartNew(async () =>
+                {
+                    while (true)
+                    {
+                        await Task.Delay(1000);
+                        if (PixivApi != null)
+                        {
+                            if ((PixivApiAuthExpiresIn - DateTimeOffset.Now).TotalSeconds <= 60)
+                            {
+                                (var authTime, var authResponse) = await PixivApi.AuthAsync(PixivApiAuthResponse.RefreshToken);
+                                UpdatePixivApiToken(authTime, authResponse);
+                                ConsoleLog.Info("Pixiv", "已更新Pixiv Api登录信息");
+                            }
                         }
                     }
                 });
             }
         }
 
-        static void KeepPixivApiAuth()
-        {
-            Task.Factory.StartNew(async () =>
-            {
-                while (true)
-                {
-                    await Task.Delay(1000);
-                    if (PixivApi != null)
-                    {
-                        if ((PixivApiAuthExpiresIn - DateTimeOffset.Now).TotalSeconds <= 60)
-                        {
-                            (var authTime, var authResponse) = await PixivApi.AuthAsync(PixivApiAuthResponse.RefreshToken);
-                            UpdatePixivApiToken(authTime, authResponse);
-                            ConsoleLog.Info("Pixiv", "已更新Pixiv Api登录信息");
-                        }
-                    }
-                }
-            });
-        }
+        public static Config Config { get; private set; }
 
         public static WebProxy WebProxy { get; private set; }
 
-        public static Config Config { get; private set; }
+        public static QQService QQServer { get; private set; }
+        public static DiscordAPP DiscordServer { get; private set; }
+
+        public static PixivApiClient PixivApi => PixivAccountClient.PixivApi;
+        public static string PixivApiToken => PixivAccountClient.PixivApiToken;
 
         public static bool Working = false;
 
         static DateTime LaunchTime;
 
-#pragma warning disable IDE0060 // 删除未使用的参数
-        static void Main(string[] args)
-#pragma warning restore IDE0060 // 删除未使用的参数
+        static void Main()
         {
-            AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
-            {
-                var directory = Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "crash-report"));
-                var now = DateTime.Now;
-                File.WriteAllText(
-                    Path.Combine(directory.FullName, $"crash-{now:yyyyMMdd-HHmmss-ffff}.log"),
-                    new StringBuilder()
-                    .AppendLine($"[启动于 {LaunchTime:yyyy-MM-dd HH:mm:ss.ffff}]")
-                    .AppendLine($"[崩溃于 {now:yyyy-MM-dd HH:mm:ss.ffff}]")
-                    .AppendLine($"[工作时长 {now - LaunchTime}]")
-                    .Append(ConsoleLog.ErrorLogBuilder((Exception)args.ExceptionObject))
-                    .ToString());
-            };
-            LaunchTime = DateTime.Now;
             Console.Title = "Ritsukage Core";
-            ConsoleLog.Info("Main", "Loading...");
-            Launch();
-            while (Working)
+            var currentProcess = Process.GetCurrentProcess();
+            _ = new Mutex(true, currentProcess.ProcessName, out var first);
+            if (first)
             {
-                UpdateTitle();
-                Thread.Sleep(100);
+                LaunchTime = DateTime.Now;
+                ConsoleLog.Info("Main", "Loading...");
+                InitUnhandledExceptionHandler();
+                InitWatchDog(currentProcess.Id, currentProcess.ProcessName);
+                Launch();
+                while (Working)
+                {
+                    UpdateTitle();
+                    Thread.Sleep(100);
+                }
+                Shutdown();
             }
-            Shutdown();
+            else
+            {
+                ConsoleLog.Error("Ritsukage Core", "本程序目前不支持同一架构多实例同时运行");
+                return;
+            }
             ConsoleLog.Info("Main", "程序主逻辑已结束，按任意键结束程序");
             Console.ReadKey();
-        }
-
-        static void UpdateTitle() => Console.Title = $"Ritsukage Core | 启动于 {LaunchTime:yyyy-MM-dd HH:mm:ss} | 运行时长 {DateTime.Now - LaunchTime}"
-            + (Config.IsDebug ? " | DEBUG MODE" : string.Empty);
-
-        static void SetHttpProxy(string url)
-        {
-            WebProxy = new WebProxy(url, true);
-            WebRequest.DefaultWebProxy = WebProxy;
         }
 
         static void Launch()
@@ -219,7 +212,7 @@ namespace Ritsukage
                 ConsoleLog.Info("Main", "Roll Api 已初始化");
             }
 
-            Task.Run(PixivApiLogin);
+            Task.Run(PixivAccountClient.PixivApiLogin);
 
             if (cfg.Discord)
             {
@@ -291,6 +284,65 @@ namespace Ritsukage
             catch
             {
             }
+        }
+
+        static void SetHttpProxy(string url)
+        {
+            WebProxy = new WebProxy(url, true);
+            WebRequest.DefaultWebProxy = WebProxy;
+        }
+
+        static void UpdateTitle() => Console.Title = $"Ritsukage Core | 启动于 {LaunchTime:yyyy-MM-dd HH:mm:ss} | 运行时长 {DateTime.Now - LaunchTime}"
+            + (Config.IsDebug ? " | DEBUG MODE" : string.Empty);
+
+        static bool _initedUnhandledExceptionHandler = false;
+        static void InitUnhandledExceptionHandler()
+        {
+            if (_initedUnhandledExceptionHandler) return;
+            _initedUnhandledExceptionHandler = true;
+            AppDomain.CurrentDomain.UnhandledException += UnhandledExceptionHandler;
+        }
+
+        static void UnhandledExceptionHandler(object sender, UnhandledExceptionEventArgs args)
+        {
+            var directory = Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "crash-report"));
+            var now = DateTime.Now;
+            File.WriteAllText(
+                Path.Combine(directory.FullName, $"crash-{now:yyyyMMdd-HHmmss-ffff}.log"),
+                new StringBuilder()
+                .AppendLine($"[启动于 {LaunchTime:yyyy-MM-dd HH:mm:ss.ffff}]")
+                .AppendLine($"[崩溃于 {now:yyyy-MM-dd HH:mm:ss.ffff}]")
+                .AppendLine($"[工作时长 {now - LaunchTime}]")
+                .Append(ConsoleLog.ErrorLogBuilder((Exception)args.ExceptionObject))
+                .ToString());
+        }
+
+        static bool _initedWatchDog = false;
+        static void InitWatchDog(int pid, string name)
+        {
+            if (_initedWatchDog) return;
+            _initedWatchDog = true;
+            ConsoleLog.Info("Main", "初始化进程监视器……");
+            var process = Process.Start(new ProcessStartInfo("SimpleWatchDog.exe", $"{pid} -n \"{name}\"")
+            {
+                UseShellExecute = false
+            });
+            SimpleWatchDog.SimpleIPC.Client ipcClient = new(name);
+            var delay = TimeSpan.FromSeconds(20);
+            new Thread(() =>
+            {
+                while (!Working)
+                    Thread.Sleep(100);
+                while (Working)
+                {
+                    ipcClient.SendMessage("HeartBeat");
+                    Thread.Sleep(delay);
+                }
+            })
+            {
+                IsBackground = true
+            }.Start();
+            ConsoleLog.Info("Main", "进程监视器初始化完成");
         }
     }
 }
---
+++ SimpleWatchDog/Console/ConsoleLog.cs
@@ -0,0 +1,170 @@
+using System.Text;
+
+namespace SimpleWatchDog.Console
+{
+    /// <summary>
+    /// <para>控制台格式化Log类</para>
+    /// <para>用于输出格式化Log</para>
+    /// </summary>
+    public static class ConsoleLog
+    {
+        #region Log等级设置
+        private static LogLevel Level = LogLevel.Info;
+
+        /// <summary>
+        /// 设置日志等级
+        /// </summary>
+        /// <param name="level">LogLevel</param>
+        public static void SetLogLevel(LogLevel level) => Level = level;
+
+        /// <summary>
+        /// 禁用log
+        /// </summary>
+        public static void SetNoLog() => Level = (LogLevel)5;
+        #endregion
+
+        #region 控制台锁
+        private static readonly object ConsoleWriterLock = new();
+        #endregion
+
+        #region 格式化错误Log
+        static string FormatException(Exception e)
+            => new StringBuilder()
+            .AppendLine("Error:" + e.GetType().FullName)
+            .AppendLine("Message:" + e.Message)
+            .AppendLine("Stack Trace:")
+            .Append(e.StackTrace).ToString();
+
+        /// <summary>
+        /// 生成格式化的错误Log文本
+        /// </summary>
+        /// <param name="e">错误</param>
+        /// <returns>格式化Log</returns>
+        public static string ErrorLogBuilder(Exception e, bool showInnerException)
+        {
+            var sb = new StringBuilder().AppendLine()
+            .AppendLine("==============ERROR==============")
+            .AppendLine(FormatException(e));
+            if (showInnerException)
+            {
+                while (e.InnerException != null)
+                {
+                    sb.AppendLine("==============INNER==============")
+                        .AppendLine(FormatException(e.InnerException));
+                    e = e.InnerException;
+                }
+            }
+            sb.Append("=================================");
+            return sb.ToString();
+        }
+        public static string ErrorLogBuilder(Exception e) => ErrorLogBuilder(e, false);
+        public static string GetFormatString(this Exception e, bool showInnerException) => ErrorLogBuilder(e, showInnerException);
+        public static string GetFormatString(this Exception e) => ErrorLogBuilder(e);
+        #endregion
+
+        #region 格式化控制台Log函数
+        /// <summary>
+        /// 向控制台发送Info信息
+        /// </summary>
+        /// <param name="type">类型</param>
+        /// <param name="message">信息内容</param>
+        public static void Info(object type, object message)
+        {
+            if (Level > LogLevel.Info) return;
+            lock (ConsoleWriterLock)
+            {
+                System.Console.ForegroundColor = ConsoleColor.White;
+                System.Console.WriteLine($"[{DateTime.Now}][INFO][{type}]{message}");
+            }
+        }
+
+        /// <summary>
+        /// 向控制台发送Warning信息
+        /// </summary>
+        /// <param name="type">类型</param>
+        /// <param name="message">信息内容</param>
+        public static void Warning(object type, object message)
+        {
+            if (Level > LogLevel.Warn) return;
+            lock (ConsoleWriterLock)
+            {
+                System.Console.ForegroundColor = ConsoleColor.White;
+                System.Console.Write($"[{DateTime.Now}][");
+                System.Console.ForegroundColor = ConsoleColor.Yellow;
+                System.Console.Write("WARNINIG");
+                System.Console.ForegroundColor = ConsoleColor.White;
+                System.Console.Write($"][{type}]");
+                System.Console.ForegroundColor = ConsoleColor.Yellow;
+                System.Console.WriteLine($"{message}");
+                System.Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+
+        /// <summary>
+        /// 向控制台发送Error信息
+        /// </summary>
+        /// <param name="type">类型</param>
+        /// <param name="message">信息内容</param>
+        public static void Error(object type, object message)
+        {
+            if (Level > LogLevel.Error) return;
+            lock (ConsoleWriterLock)
+            {
+                System.Console.ForegroundColor = ConsoleColor.White;
+                System.Console.Write($"[{DateTime.Now}][");
+                System.Console.ForegroundColor = ConsoleColor.Red;
+                System.Console.Write("ERROR");
+                System.Console.ForegroundColor = ConsoleColor.White;
+                System.Console.Write($"][{type}]");
+                System.Console.ForegroundColor = ConsoleColor.Red;
+                System.Console.WriteLine(message);
+                System.Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+
+        /// <summary>
+        /// 向控制台发送Fatal信息
+        /// </summary>
+        /// <param name="type">类型</param>
+        /// <param name="message">信息内容</param>
+        public static void Fatal(object type, object message)
+        {
+            if (Level > LogLevel.Fatal) return;
+            lock (ConsoleWriterLock)
+            {
+                System.Console.ForegroundColor = ConsoleColor.White;
+                System.Console.Write($"[{DateTime.Now}][");
+                System.Console.ForegroundColor = ConsoleColor.DarkRed;
+                System.Console.Write("FATAL");
+                System.Console.ForegroundColor = ConsoleColor.White;
+                System.Console.Write($"][{type}]");
+                System.Console.ForegroundColor = ConsoleColor.DarkRed;
+                System.Console.WriteLine(message);
+                System.Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+
+        /// <summary>
+        /// 向控制台发送Debug信息
+        /// </summary>
+        /// <param name="type">类型</param>
+        /// <param name="message">信息内容</param>
+        public static void Debug(object type, object message)
+        {
+            if (Level != LogLevel.Debug) return;
+            lock (ConsoleWriterLock)
+            {
+                System.Console.ForegroundColor = ConsoleColor.White;
+                System.Console.Write($"[{DateTime.Now}][");
+                System.Console.ForegroundColor = ConsoleColor.Cyan;
+                System.Console.Write("DEBUG");
+                System.Console.ForegroundColor = ConsoleColor.White;
+                System.Console.Write($"][{type}]");
+                System.Console.ForegroundColor = ConsoleColor.Cyan;
+                System.Console.WriteLine(message);
+                System.Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+        #endregion
+    }
+}
